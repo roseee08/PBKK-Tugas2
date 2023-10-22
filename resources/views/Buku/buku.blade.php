acfaf42afe89760a61310db65fec68d40dcdb913
rose <roseenjellina@gmail.com>
2023-10-22 20:02:05 +0700

    commit file

---
+++ resources/views/Buku/buku.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Buku</title>
+    <link rel="stylesheet" href="assets/buku.css">
+</head>
+<body style="background-color: rgb(254, 255, 240);">
+
+<h1>Data Buku</h1>
+
+<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>Judul Buku</th>
+        <th>Pengarang</th>
+        <th>Tahun Terbit</th>
+        <th>Penerbit</th>
+        <th>Cover</th>
+    </tr>
+    <tr>
+        <td>A01</td>
+        <td>Hujan Bulan Juni</td>
+        <td>Sapardi Djoko Damono</td>
+        <td>2015</td>
+        <td>Grasindo</td>
+        <td><img src="assets/image/Hujan.jpeg" alt="Hujan Bulan JUni" </td>
+    </tr>
+    <tr>
+        <td>A02</td>
+        <td>My Ice Girl</td>
+        <td>Pit Sansi</td>
+        <td>2018</td>
+        <td>Bentang Belia</td>
+        <td><img src="assets/image/Myice.jpeg" alt="My Ice Girl" </td>
+    </tr>
+    <tr>
+        <td>A03</td>
+        <td>Merbaby</td>
+        <td>Liara Audina</td>
+        <td>2022</td>
+        <td>namina books</td>
+        <td><img src="assets/image/Merbaby.jpg" alt="Merbaby" </td>
+    </tr>     
+         <td>A04</td>
+         <td>A+</td>
+         <td> Ananda Putri </td>
+         <td>2022</td>
+         <td>Grasindo</td>
+         <td><img src="assets/image/A.jpeg" alt="A+" </td>
+    </tr>
+         <td>A05</td>
+         <td>Moon- Bumi series</td>
+         <td>Tere Liye</td>
+         <td>2015</td>
+         <td>Gramedia Pustaka Utama</td>
+         <td><img src="assets/image/Moon.jpg" alt="Moon" </td>
+    </tr>
+         <td>A05</td>
+         <td>Jingga dan Senja</td>
+         <td>Esti Kinasih</td>
+         <td>2013</td>
+         <td>Gramedia Pustaka Utama</td>
+         <td><img src="assets/image/Jingga.jpeg" alt="Jingga dan Senja" </td>
+    </tr>
+  </table>             
+</body>
+</html>
---
+++ resources/views/Siswa/siswa.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Nama Siswa</title>
+    <link rel="stylesheet" href="{{ asset('css/siswa.css') }}">
+
+</head>
+
+<body>
+    <h1>Daftar Nama Siswa SMAN 1 Bengkulu Tengah</h1>
+
+<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>Nama</th>
+        <th>Kelas</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Siska</td>
+        <td>XII-MIPA 1</td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>Aulia</td>
+        <td>XI-IPS 1</td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>Febrian</td>
+        <td>XII-MIPA 2</td>
+    </tr>
+    <tr>
+        <td>4</td>
+        <td>Marco</td>
+        <td>X-Bahasa</td>
+    <tr>
+        <td>5</td>
+        <td>Najwa</td>
+        <td>XII-MIPA 3</td>
+    </tr>
+</body>
+</html>
